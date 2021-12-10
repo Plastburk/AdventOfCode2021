663a92d607b69ee124b13b307d69b4aede761e7a
Anton <anton.hesselbom@gmail.com>
2021-12-10 19:44:28 +0100

    Day9 optimizations

--- AdventOfCode2021/Code/Day9.cpp
+++ AdventOfCode2021/Code/Day9.cpp
@@ -1,7 +1,6 @@
 #include "Day9.h"
 #include "Utilities/Utilities.h"
 
-#include <stack>
 #include <set>
 
 void Day9::ReadInput(std::ifstream& stream)
@@ -22,12 +21,9 @@ struct Vec2D
 	{	}
 };
 
-inline uint8_t GetValue(const std::vector<uint8_t>& input, Vec2D pos, Vec2D size)
+inline uint8_t GetValue(const std::vector<uint8_t>& input, int x, int y, int width)
 {
-	if (pos.X < 0 || pos.X >= size.X || pos.Y < 0 || pos.Y >= size.Y)
-		return 10;
-
-	int i = pos.Y * size.X + pos.X;
+	int i = y * width + x;
 	return input[i];
 }
 
@@ -38,51 +34,48 @@ int Day9::RunA()
 	int inputSize = (int)input.size() - 1;
 	int width = input[inputSize];
 	int height = inputSize / width;
-	Vec2D size(width, inputSize / width);
 
 	for (int y = 0; y < height; y++)
 		for (int x = 0; x < width; x++)
 		{
-			int top = GetValue(input, Vec2D(x, y - 1), size);
-			int left = GetValue(input, Vec2D(x - 1, y), size);
-			int self = GetValue(input, Vec2D(x, y), size);
-			int right = GetValue(input, Vec2D(x + 1, y), size);
-			int bottom = GetValue(input, Vec2D(x, y + 1), size);
+			int self = GetValue(input, x, y, width);
 
-			if (self < top && self < left && self < right && self < bottom)
+			if ((y == 0 || GetValue(input, x, y - 1, width) > self) &&
+				(x == 0 || GetValue(input, x - 1, y, width) > self) &&
+				(x == width - 1 || GetValue(input, x + 1, y, width) > self) &&
+				(y == height - 1 || GetValue(input, x, y + 1, width) > self))
 				total += self + 1;
 		}
 
 	return total;
 }
 
-inline int PosToIndex(size_t x, size_t y, size_t width)
+inline int TraverseBasin(std::vector<uint8_t>& input, size_t i, size_t width, size_t height)
 {
-	return (int)(y * width + x);
-}
-
-inline int TraverseBasin(std::vector<uint8_t>& input, size_t i, size_t width, size_t height, size_t inputSize)
-{
-	std::stack<Vec2D> stack;
-	stack.push(Vec2D(int(i % width), int(i / width)));
+	std::vector<Vec2D> stack;
+	stack.push_back(Vec2D(int(i % width), int(i / width)));
 
 	int basinSize = 0;
 	while (!stack.empty())
 	{
-		Vec2D pos = stack.top();
-		stack.pop();
+		Vec2D pos = stack.back();
+		stack.pop_back();
 
 		size_t i = pos.Y * width + pos.X;
-		if (pos.X < 0 || pos.X >= width || pos.Y < 0 || pos.Y >= height || input[i] >= 9)
+		if (input[i] == 9)
 			continue;
 
 		basinSize++;
 		input[i] = 9;
 
-		stack.push(Vec2D(pos.X, pos.Y - 1));
-		stack.push(Vec2D(pos.X - 1, pos.Y));
-		stack.push(Vec2D(pos.X + 1, pos.Y));
-		stack.push(Vec2D(pos.X, pos.Y + 1));
+		if (pos.Y > 0)
+			stack.push_back(Vec2D(pos.X, pos.Y - 1));
+		if (pos.X > 0)
+			stack.push_back(Vec2D(pos.X - 1, pos.Y));
+		if (pos.X < width - 1)
+			stack.push_back(Vec2D(pos.X + 1, pos.Y));
+		if (pos.Y < height - 1)
+			stack.push_back(Vec2D(pos.X, pos.Y + 1));
 	}
 
 	return basinSize;
@@ -99,7 +92,7 @@ int Day9::RunB()
 	{
 		if (input[i] < 9)
 		{
-			int size = TraverseBasin(input, i, width, height, inputSize);
+			int size = TraverseBasin(input, i, width, height);
 			if (largest.size() < 3)
 				largest.insert(size);
 			else if (size > *largest.begin())
