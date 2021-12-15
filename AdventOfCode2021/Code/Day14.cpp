3b1c507420ba873cdf9d21ec997b139b55189048
Anton <anton.hesselbom@gmail.com>
2021-12-15 19:38:52 +0100

    Day14 optimized

--- AdventOfCode2021/Code/Day14.cpp
+++ AdventOfCode2021/Code/Day14.cpp
@@ -1,47 +1,18 @@
 #include "Day14.h"
 #include "Utilities/Utilities.h"
 
-#include <functional>
-
-struct PolymerPair
-{
-	char A;
-	char B;
-
-	PolymerPair() :
-		A(0), B(0)
-	{	}
-
-	PolymerPair(char a, char b) :
-		A(a), B(b)
-	{	}
-
-	bool operator==(const PolymerPair& other) const
-	{
-		return A == other.A && B == other.B;
-	}
-};
-
-template<>
-struct std::hash<PolymerPair>
-{
-	size_t operator()(const PolymerPair& value) const
-	{
-		return ((size_t)value.A) << 32 | (size_t)value.B;
-	}
-};
-
 struct PolymerOutcome
 {
-	PolymerPair A;
-	PolymerPair B;
+	int IndexA;
+	int IndexB;
+	int NewElementIndex;
 
 	PolymerOutcome() :
-		A(), B()
+		IndexA(), IndexB(), NewElementIndex()
 	{	}
 
-	PolymerOutcome(const PolymerPair& a, const PolymerPair& b) :
-		A(a), B(b)
+	PolymerOutcome(int indexA, int indexB, int newElementIndex) :
+		IndexA(indexA), IndexB(indexB), NewElementIndex(newElementIndex)
 	{	}
 };
 
@@ -56,56 +27,66 @@ void Day14::ReadInput(std::ifstream& stream)
 	);
 }
 
-uint64_t Run(const std::vector<char>& input1, const std::vector<char>& input2, int iterations)
+inline int PairToIndex(char a, char b)
 {
-	std::unordered_map<PolymerPair, uint64_t> pairs;
-	std::unordered_map<PolymerPair, PolymerOutcome> outcomes;
+	return (a - 'A') * 26 + (b - 'A');
+}
+
+inline int ElementToIndex(char element)
+{
+	return element - 'A';
+}
+
+inline uint64_t Run(const std::vector<char>& input1, const std::vector<char>& input2, int iterations)
+{
+	const int maxAmountOfPairs = 26 * 26;
+	uint64_t pairs[maxAmountOfPairs]{};
+	PolymerOutcome outcomes[maxAmountOfPairs]{};
 
 	for (size_t i = 0; i < input2.size(); i += 3)
 	{
 		char c1 = input2[i];
 		char c2 = input2[i + 1];
 		char c3 = input2[i + 2];
-		PolymerPair pair(c1, c2);
-		outcomes.emplace(pair, PolymerOutcome(PolymerPair(c1, c3), PolymerPair(c3, c2)));
+		outcomes[PairToIndex(c1, c2)] = PolymerOutcome(PairToIndex(c1, c3), PairToIndex(c3, c2), ElementToIndex(c3));
 	}
 
-	std::unordered_map<char, uint64_t> elementCount;
+	uint64_t elementCount[26]{};
 	for (size_t i = 0; i < input1.size() - 1; i++)
 	{
-		PolymerPair pair(input1[i], input1[i + 1]);
-		pairs[pair]++;
-
-		elementCount[input1[i]]++;
+		pairs[PairToIndex(input1[i], input1[i + 1])]++;
+		elementCount[ElementToIndex(input1[i])]++;
 	}
-	elementCount[input1[input1.size() - 1]]++;
+	elementCount[ElementToIndex(input1[input1.size() - 1])]++;
 
 	for (int i = 0; i < iterations; i++)
 	{
-		std::unordered_map<PolymerPair, uint64_t> prePairs(pairs);
-		for (auto& pair : prePairs)
+		uint64_t previousPairs[maxAmountOfPairs];
+		memcpy(previousPairs, pairs, sizeof(uint64_t) * maxAmountOfPairs);
+		for (int j = 0; j < maxAmountOfPairs; j++)
 		{
-			if (pair.second == 0)
+			uint64_t pairAmount = previousPairs[j];
+			if (pairAmount == 0)
 				continue;
-			uint64_t amount = prePairs[pair.first];
-			pairs[pair.first] -= amount;
+			pairs[j] -= pairAmount;
 
-			const auto& outcome = outcomes[pair.first];
-			pairs[outcome.A] += amount;
-			pairs[outcome.B] += amount;
-			elementCount[outcome.A.B] += amount;
+			const auto& outcome = outcomes[j];
+			pairs[outcome.IndexA] += pairAmount;
+			pairs[outcome.IndexB] += pairAmount;
+			elementCount[outcome.NewElementIndex] += pairAmount;
 		}
 	}
 
 	uint64_t min = UINT64_MAX;
 	uint64_t max = 0;
-	for (auto& element : elementCount)
+	for (uint64_t element : elementCount)
 	{
-		uint64_t value = element.second;
-		if (value < min)
-			min = value;
-		if (value > max)
-			max = value;
+		if (element == 0)
+			continue;
+		if (element < min)
+			min = element;
+		if (element > max)
+			max = element;
 	}
 
 	return max - min;
