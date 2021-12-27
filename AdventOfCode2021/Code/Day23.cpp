c3487788126ffebb4f3c7438c58abdf3e52b8e0d
Anton <anton.hesselbom@gmail.com>
2021-12-27 14:59:57 +0100

    Day23 optimized

--- AdventOfCode2021/Code/Day23.cpp
+++ AdventOfCode2021/Code/Day23.cpp
@@ -41,66 +41,29 @@ void Day23::ReadInput(std::ifstream& stream)
 template <int RoomSize>
 using RoomsState = std::array<uint8_t, 11 + (4 * RoomSize)>;
 
-template<>
-struct std::hash<RoomsState<2>>
+template <int RoomSize>
+inline uint64_t StateToHash(const RoomsState<RoomSize>& state)
 {
-	size_t operator()(const RoomsState<2>& value) const
-	{
-		size_t hash = 0;
-		for (int i = 0; i < 19; i++)
-			hash = hash * 5 + value[i];
-		return hash;
-	}
-};
+	int length = 11 + (RoomSize * 4);
 
-template<>
-struct std::hash<RoomsState<4>>
-{
-	size_t operator()(const RoomsState<4>& value) const
-	{
-		size_t hash = 0;
-		for (int i = 0; i < 27; i++)
-			hash = hash * 5 + value[i];
-		return hash;
-	}
-};
-
-// TODO Remove
-inline char UnitToChar(uint8_t unit)
-{
-	if (unit == 0)
-		return '.';
-	else
-		return 'A' + (unit - 1);
+	uint64_t hash = 0;
+	for (int i = 0; i < length; i++)
+		hash = hash * 5 + state[i];
+	return hash;
 }
 
-// TODO Remove
 template <int RoomSize>
-inline void PrintState(const RoomsState<RoomSize>& state)
+inline RoomsState<RoomSize> HashToState(uint64_t hash)
 {
-	printf("\n");
-	printf("#############\n");
-	printf("#");
-	for (int i = 0; i < 11; i++)
-		printf("%c", UnitToChar(state[i]));
-	printf("#\n");
-	if (RoomSize == 2)
-	{
-		printf("###%c#%c#%c#%c###\n", UnitToChar(state[11]), UnitToChar(state[13]), UnitToChar(state[15]), UnitToChar(state[17]));
-		printf("  #%c#%c#%c#%c#  \n", UnitToChar(state[12]), UnitToChar(state[14]), UnitToChar(state[16]), UnitToChar(state[18]));
-	}
-	else
-	{
+	int length = 11 + (RoomSize * 4);
 
-#pragma warning(push)
-#pragma warning(disable:28020)
-		printf("###%c#%c#%c#%c###\n", UnitToChar(state[11]), UnitToChar(state[15]), UnitToChar(state[19]), UnitToChar(state[23]));
-		printf("  #%c#%c#%c#%c#  \n", UnitToChar(state[12]), UnitToChar(state[16]), UnitToChar(state[20]), UnitToChar(state[24]));
-		printf("  #%c#%c#%c#%c#  \n", UnitToChar(state[13]), UnitToChar(state[17]), UnitToChar(state[21]), UnitToChar(state[25]));
-		printf("  #%c#%c#%c#%c#  \n", UnitToChar(state[14]), UnitToChar(state[18]), UnitToChar(state[22]), UnitToChar(state[26]));
-#pragma warning(pop)
+	RoomsState<RoomSize> state;
+	for (int i = length - 1; i >= 0; i--)
+	{
+		state[i] = hash % 5;
+		hash /= 5;
 	}
-	printf("  #########  \n");
+	return state;
 }
 
 template <int RoomSize>
@@ -108,7 +71,7 @@ struct State
 {
 	int GCost;
 	int FCost;
-	RoomsState<RoomSize> Slots;
+	uint64_t Slots;
 };
 
 template<int RoomSize>
@@ -124,7 +87,10 @@ template <int RoomSize>
 using MinQueue = std::priority_queue<State<RoomSize>, std::vector<State<RoomSize>>, std::greater<State<RoomSize>>>;
 
 template <int RoomSize>
-using VisitedMap = robin_hood::unordered_map<RoomsState<RoomSize>, int>;
+using VisitedMap = robin_hood::unordered_map<uint64_t, int>;
+
+constexpr std::array<uint8_t, 7> ValidHallwayPositions = { 0, 1, 3, 5, 7, 9, 10 };
+constexpr std::array<int, 4> CostPerUnitType = { 1, 10, 100, 1000 };
 
 template <int RoomSize>
 inline bool IsPathFree(const RoomsState<RoomSize>& state, int from, int to)
@@ -132,28 +98,101 @@ inline bool IsPathFree(const RoomsState<RoomSize>& state, int from, int to)
 	int leftSlot = std::min(from, to);
 	int rightSlot = std::max(from, to);
 
-	if (leftSlot == from)
-		leftSlot++;
-
-	if (rightSlot == from)
-		rightSlot--;
-
 	for (int i = leftSlot; i <= rightSlot; i++)
-		if (state[i] != 0)
+		if (i != from && state[i] != 0)
 			return false;
 
 	return true;
 }
 
 template <int RoomSize>
+inline int CalculateHeuristic(const RoomsState<RoomSize>& state)
+{
+	int heuristic = 0;
+
+	// Calculate cost for all units that still need to leave their rooms
+	// (This calculates cost to leave their room and go to the hallway slot right above their target room)
+	for (int room = 0; room < 4; room++)
+	{
+		int firstRoomSlot = 11 + (room * RoomSize);
+		int lastRoomSlot = firstRoomSlot + (RoomSize - 1);
+
+		// Check if any units in this room needs to move
+		bool hasAnyWrongUnits = false;
+		int firstUnitSlot = -1;
+		int lastWrongUnitSlot = -1;
+		for (int i = firstRoomSlot; i <= lastRoomSlot; i++)
+		{
+			if (state[i] != 0 && state[i] != room + 1)
+				lastWrongUnitSlot = i;
+			if (firstUnitSlot == -1 && state[i] != 0)
+				firstUnitSlot = i;
+		}
+
+		if (lastWrongUnitSlot != -1)
+		{
+			for (int i = firstUnitSlot; i <= lastWrongUnitSlot; i++)
+			{
+#pragma warning(push)
+#pragma warning(disable:28020)
+				uint8_t unitType = state[i];
+#pragma warning(pop)
+
+				// Distance to leave room and go into hallway
+				int distance = (firstUnitSlot - firstRoomSlot + 1);
+
+				// Distance to go to correct hallway slot
+				int targetRoom = unitType - 1;
+
+				int hallwayOutsideRoomSlot = 2 + (room * 2);
+				int hallwayOutsideTargetRoomSlot = 2 + (targetRoom * 2);
+
+				distance += std::abs(hallwayOutsideTargetRoomSlot - hallwayOutsideRoomSlot);
+
+				// Add cost
+				heuristic += CostPerUnitType[unitType - 1] * distance;
+			}
+
+			// Calculate cost to enter the correct room for all units that are not in the correct room
+			{
+				int unitsThatNeedToMoveIn = lastWrongUnitSlot - firstRoomSlot + 1;
+				heuristic = CostPerUnitType[room] * (((unitsThatNeedToMoveIn * unitsThatNeedToMoveIn) + unitsThatNeedToMoveIn) / 2);
+			}
+		}
+	}
+
+	// Calculate cost for all units that are in the hallway
+	// (This calculates cost to go to the hallway slot right above their target room)
+	for (int i = 0; i < 11; i++)
+	{
+#pragma warning(push)
+#pragma warning(disable:28020)
+		uint8_t unitType = state[i];
+#pragma warning(pop)
+
+		if (unitType == 0)
+			continue;
+
+		// Distance to go to correct hallway slot
+		int targetRoom = unitType - 1;
+		int hallwayOutsideTargetRoomSlot = 2 + (targetRoom * 2);
+
+		int distance = std::abs(hallwayOutsideTargetRoomSlot - i);
+
+		// Add cost
+		heuristic += CostPerUnitType[unitType - 1] * distance;
+	}
+
+	return heuristic;
+}
+
+template <int RoomSize>
 inline void AddAllValidMoves(const State<RoomSize>& state, VisitedMap<RoomSize>& visited, MinQueue<RoomSize>& openList)
 {
-	constexpr std::array<uint8_t, 7> ValidHallwayPositions = { 0, 1, 3, 5, 7, 9, 10 };
-	constexpr std::array<int, 4> CostPerUnitType = { 1, 10, 100, 1000 };
-
+	const auto& slots = HashToState<RoomSize>(state.Slots);
 	for (int pos : ValidHallwayPositions)
 	{
-		if (state.Slots[pos] == 0)
+		if (slots[pos] == 0)
 		{
 			// Generate all valid moves from an initial room slot to this hallway slot
 			for (int room = 0; room < 4; room++)
@@ -166,9 +205,9 @@ inline void AddAllValidMoves(const State<RoomSize>& state, VisitedMap<RoomSize>&
 				int firstUnitSlot = -1;
 				for (int i = firstRoomSlot; i <= lastRoomSlot; i++)
 				{
-					if (state.Slots[i] != 0 && state.Slots[i] != room + 1)
+					if (slots[i] != 0 && slots[i] != room + 1)
 						hasAnyWrongUnits = true;
-					if (firstUnitSlot == -1 && state.Slots[i] != 0)
+					if (firstUnitSlot == -1 && slots[i] != 0)
 						firstUnitSlot = i;
 				}
 
@@ -178,37 +217,36 @@ inline void AddAllValidMoves(const State<RoomSize>& state, VisitedMap<RoomSize>&
 				// Check if there is any units in the way from this room to the target slot
 				int hallwayOutsideRoomSlot = 2 + (room * 2);
 				int targetHallwaySlot = pos;
-				
-				if (!IsPathFree<RoomSize>(state.Slots, hallwayOutsideRoomSlot, targetHallwaySlot))
+
+				if (!IsPathFree<RoomSize>(slots, hallwayOutsideRoomSlot, targetHallwaySlot))
 					continue;
 
 				// Generate new state
-				auto newState = state.Slots;
+				auto newState = slots;
 #pragma warning(push)
 #pragma warning(disable:28020)
-				uint8_t unitType = state.Slots[firstUnitSlot];
+				uint8_t unitType = slots[firstUnitSlot];
 #pragma warning(pop)
 				newState[targetHallwaySlot] = unitType;
 				newState[firstUnitSlot] = 0;
+				uint64_t newStateHash = StateToHash<RoomSize>(newState);
 
 				// Add to openList & visited, but only if current cost in visited is higher than new cost, or if visited doesn't contain this node
 				int distance = std::abs(targetHallwaySlot - hallwayOutsideRoomSlot) + (firstUnitSlot - firstRoomSlot + 1);
 				int newCost = state.GCost + distance * CostPerUnitType[unitType - 1];
-				auto visit = visited.find(newState);
+				auto visit = visited.find(newStateHash);
 				if (visit == visited.end() || visit->second > newCost)
 				{
-					visited[newState] = newCost;
-					int heuristic = 0; // TODO Add Heuristic
-					openList.push({ newCost, newCost + heuristic, newState });
-
-					//PrintState(newState);
+					visited[newStateHash] = newCost;
+					int heuristic = CalculateHeuristic<RoomSize>(newState);
+					openList.push({ newCost, newCost + heuristic, newStateHash });
 				}
 			}
 		}
 		else
 		{
 			// Generate all valid moves from this hallway slot to final room slot
-			uint8_t unitType = state.Slots[pos];
+			uint8_t unitType = slots[pos];
 			int room = unitType - 1;
 			int firstRoomSlot = 11 + (room * RoomSize);
 			int lastRoomSlot = firstRoomSlot + (RoomSize - 1);
@@ -218,9 +256,9 @@ inline void AddAllValidMoves(const State<RoomSize>& state, VisitedMap<RoomSize>&
 			int firstEmptyRoomSlot = -1;
 			for (int i = firstRoomSlot; i <= lastRoomSlot; i++)
 			{
-				if (firstEmptyRoomSlot == -1 && state.Slots[i] == 0)
+				if (firstEmptyRoomSlot == -1 && slots[i] == 0)
 					firstEmptyRoomSlot = i;
-				if (state.Slots[i] != 0 && state.Slots[i] != unitType)
+				if (slots[i] != 0 && slots[i] != unitType)
 				{
 					hasWrongUnit = true;
 					break;
@@ -232,28 +270,27 @@ inline void AddAllValidMoves(const State<RoomSize>& state, VisitedMap<RoomSize>&
 
 			// Check if path to room is free
 			int hallwayOutsideRoomSlot = 2 + (room * 2);
-			if (!IsPathFree<RoomSize>(state.Slots, pos, hallwayOutsideRoomSlot))
+			if (!IsPathFree<RoomSize>(slots, pos, hallwayOutsideRoomSlot))
 				continue;
 
 			// Generate new state
-			auto newState = state.Slots;
+			auto newState = slots;
 #pragma warning(push)
 #pragma warning(disable:28020)
 			newState[firstEmptyRoomSlot] = unitType;
 #pragma warning(pop)
 			newState[pos] = 0;
+			uint64_t newStateHash = StateToHash<RoomSize>(newState);
 
 			// Add to openList & visited, but only if current cost in visited is higher than new cost, or if visited doesn't contain this node
 			int distance = std::abs(hallwayOutsideRoomSlot - pos) + (firstEmptyRoomSlot - firstRoomSlot + 1);
 			int newCost = state.GCost + distance * CostPerUnitType[unitType - 1];
-			auto visit = visited.find(newState);
+			auto visit = visited.find(newStateHash);
 			if (visit == visited.end() || visit->second > newCost)
 			{
-				visited[newState] = newCost;
-				int heuristic = 0; // TODO Add Heuristic
-				openList.push({ newCost, newCost + heuristic, newState });
-
-				//PrintState(newState);
+				visited[newStateHash] = newCost;
+				int heuristic = CalculateHeuristic<RoomSize>(newState);
+				openList.push({ newCost, newCost + heuristic, newStateHash });
 			}
 		}
 	}
@@ -264,17 +301,19 @@ inline int Run(const RoomsState<RoomSize>& initialState, const RoomsState<RoomSi
 {
 	// Setup
 	VisitedMap<RoomSize> visited;
-	visited.reserve(1000); // TODO What is a good value here?
+	visited.reserve(100000);
 
 	std::vector<State<RoomSize>> queueVector;
-	queueVector.reserve(1000); // TODO What is a good value here?
+	queueVector.reserve(20000);
 	MinQueue<RoomSize> openList(std::greater<State<RoomSize>>(), queueVector);
 
 	// Add start node to openlist
-	visited[initialState] = 0;
-	openList.push({ 0, 0, initialState }); // TODO Add Heuristic
+	uint64_t initialStateHash = StateToHash<RoomSize>(initialState);
+	visited[initialStateHash] = 0;
+	openList.push({ 0, CalculateHeuristic<RoomSize>(initialState), initialStateHash });
 
-	//PrintState<RoomSize>(initialState);
+	// Prepare end state
+	uint64_t endStateHash = StateToHash<RoomSize>(endState);
 
 	bool foundPath = false;
 	while (true)
@@ -283,22 +322,16 @@ inline int Run(const RoomsState<RoomSize>& initialState, const RoomsState<RoomSi
 		auto currentState = openList.top();
 		openList.pop();
 
-		// TODO
-		//PrintState(currentState.Slots);
-
 		// If we've already visited this node with a better cost, skip it
 		if (visited[currentState.Slots] < currentState.GCost)
 			continue;
 
 		// If the node we just closed was the end node, we're done
-		if (currentState.Slots == endState)
+		if (currentState.Slots == endStateHash)
 			return currentState.GCost;
 
 		// Add all valid moves
 		AddAllValidMoves<RoomSize>(currentState, visited, openList);
-
-		// TODO
-		//printf("\n\n----------\n\n");
 	}
 }
 
@@ -333,7 +366,6 @@ int Day23::RunB()
 	input[24] = 1;
 	input[25] = 3;
 	input[26] = Input[18];
-
 
 	RoomsState<4> endState = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4 };
 
